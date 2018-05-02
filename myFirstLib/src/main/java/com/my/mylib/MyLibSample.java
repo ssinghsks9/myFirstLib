062b58e3936d984b07975a4fd7b500519fe77ea4
sandeep <sandeep.singh@incedoinc.com>
2018-05-02 12:49:55 +0530

    code

--- myFirstLib/src/main/java/com/my/mylib/MyLibSample.java
+++ myFirstLib/src/main/java/com/my/mylib/MyLibSample.java
@@ -1,8 +1,28 @@
 package com.my.mylib;
 
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+
 public class MyLibSample {
 
     public void callMethod(){
         System.out.println("MyLibSample :  callMethod called.... ");
+
+        Observable<Integer> observable = Observable.just(1, 2, 3);
+        observable.subscribe(new Observer<Integer>() {
+            @Override public void onCompleted() {
+                Log.d("Test", "In onCompleted()");
+            }
+
+            @Override public void onError(Throwable e) {
+                Log.d("Test", "In onError()");
+            }
+
+            @Override public void onNext(Integer integer) {
+                Log.d("Test", "In onNext():" + integer);
+            }
+        });
     }
 }
